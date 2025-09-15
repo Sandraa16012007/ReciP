baef71787523d5ee0d1e81f32fe47e05b019e55f
Sandraa16012007 <sandrarosaprince@gmail.com>
2025-09-16 00:54:11 +0530

    ingredients input page styles added

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
+
+// Character Counter for textarea  (ingredients page)
+const textarea = document.querySelector("textarea");
+const charCount = document.querySelector(".char-count");
+
+if (textarea && charCount) {
+  textarea.addEventListener("input", () => {
+    charCount.textContent = `${textarea.value.length}/500 characters`;
+  });
+}
